be5685967a291eea0ddd2a694aff9454c5b18f10
prateek gupta <prateekg2050@gmail.com>
2021-04-20 17:40:04 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+const app = express(); 
 
-app.get("/",(req,res)=>{
-    res.send ({bye : 'BUDDY'});
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys : [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect(keys.mongoURI);
 
-});
+
+const authRoutes = require('./routes/authRoutes');
+authRoutes(app);
+
+
+
+// app.get("/",(req,res)=>{
+//     res.send ({bye : 'BUDDY'});
+
+// });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+const users = mongoose.model('users');
+
+passport.serializeUser((user,done)=>{
+           done(null,user.id);
+})
+
+passport.deserializeUser((id,done)=>{
+    users.findById(id).then(user =>{done(null,user)});
+})
+    
+
+passport.use(
+    new GoogleStrategy(
+    {
+    clientID : keys.googleClientID,
+    clientSecret : keys.googleClientSecret,
+    callbackURL : '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done)=>{
+
+        users.findOne({ googleId: profile.id })
+        .then((existingUser)=>{
+            if(existingUser){
+                done(null,existingUser);
+                //user already exist no need to create new one
+            }
+            else{
+                //user doesnt exist , hence create one
+                new users ({ googleId: profile.id }).save().then(user =>done(null,user));
+            }
+
+        });
+        
+    }
+)
+);
